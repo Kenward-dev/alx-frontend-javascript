9586238f425ce1e3c6439088dcc3e3c197922be5
Kenward Terhemba <codewithkenward@gmail.com>
2025-09-14 09:24:08 +0100

    add print teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -43,9 +43,9 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
 console.log(printTeacher("John", "Doe"));
 console.log(printTeacher("Mary", "Smith")); 
