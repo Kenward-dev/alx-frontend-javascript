18784d8d9f4b8417b9ccb7b74c9cf0899448cd35
Kenward Terhemba <codewithkenward@gmail.com>
2025-09-14 09:17:23 +0100

    add directors interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -25,3 +25,16 @@ const teacher2: Teacher = {
 
 console.log(teacher1);
 console.log(teacher2);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
